9d1369c1a769bb03761e24fa78816247176e0276
Cremator <ilya2azin@gmail.com>
2024-04-07 18:36:26 +0300

    finish first step

---
+++ README.md
@@ -0,0 +1,41 @@
+# Hotline Scraper Usage Guide
+
+## Running the Script
+
+To run the script, use the following command in your terminal:
+
+```python main.py -s Search_Term1 Search_Term2 -p Number_of_Pages```
+
+
+- Replace `Search_Term1` and `Search_Term2` with your search queries. Use underscores (`_`) instead of spaces in your search terms.
+- Replace `Number_of_Pages` with the number of pages you want to scrape for each search term.
+
+### Examples
+
+```python main.py -s GeForce_RTX_4080_SUPER Intel_Core_i7-14700 -p 2```
+
+
+```python main.py --search GeForce_RTX_4080_SUPER Intel_Core_i7-14700 --pages 2```
+
+Default: 1 page
+
+
+This command will search for "GeForce RTX 4080 SUPER" and "Intel Core i7-14700" across 2 pages each.
+
+## Output
+
+The script outputs CSV files with the search results. Each search term generates a separate CSV file named using the pattern `hotline_search-Search_Term-Current_Time.csv`.
+
+### CSV Format
+
+The CSV files contain the following columns:
+
+- Name
+- Price
+- Link
+- Description
+- Offers
+
+## Multiple Searches
+
+You can perform multiple searches in one run by specifying multiple search terms separated by spaces (use underscores for spaces within search terms).
--- main.py
+++ main.py
@@ -5,20 +5,30 @@ import random
 import csv
 import datetime
 import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s')
+
+HOTLINE_URL = 'https://hotline.ua'
+STRFTIME = "%H-%M-%S__%d-%m-%Y"
 
 
-def get_search_info(item: str, n_pages: int):
+def get_search_info(item: str, n_pages: int, current_time: str = datetime.datetime.now().strftime(STRFTIME)):
+    _item = item.replace("_", " ")
+    logging.info(f"Starting search for '{_item}' across {n_pages} pages.")
+
     search_item = item.replace("_", "%20")
-    base_url = f'https://hotline.ua/ua/sr/?q={search_item}'
+    base_url = f'{HOTLINE_URL}/ua/sr/?q={search_item}'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    with open(f'hotline_search-{item}-{current_time}.csv', mode='w', newline='', encoding='utf-8') as file:
+    filename = f'hotline_search__{item}__{current_time}.csv'
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Name', 'Price', 'Link', 'Description', 'Offers'])
 
         for page in range(1, n_pages + 1):
             url = f"{base_url}&p={page}"
+            logging.info(f"Fetching URL: {url}")
             response = requests.get(url, headers=headers)
             soup = BeautifulSoup(response.text, 'html.parser')
             products = soup.find_all('div', class_='list-item flex')
@@ -26,7 +36,7 @@ def get_search_info(item: str, n_pages: int):
             for product in products:
                 title = product.find('a', class_='item-title text-md link link--black').text.strip()
                 price_range = product.find('div', class_='text-md text-orange text-lh--1').text.strip()
-                link = 'https://hotline.ua' + product.find('a', class_='item-title text-md link link--black')['href']
+                link = HOTLINE_URL + product.find('a', class_='item-title text-md link link--black')['href']
                 item_info = " ".join(
                     [spec.text.strip() for spec in product.find_all('span', class_='spec-item')])
                 offers_element = product.find('a', class_='link link--black text-sm m_b-5')
@@ -34,21 +44,22 @@ def get_search_info(item: str, n_pages: int):
                 writer.writerow([title, price_range, link, item_info, offers_count])
 
             time.sleep(random.uniform(1.421, 4.8943))
+    logging.info(f"Finished searching. Results saved to {filename}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="Hotline Scraper")
     parser.add_argument(
         "-s",
         "--search",
-        help="",
+        help="Search query",
         nargs="+",
         default=[]
     )
     parser.add_argument(
         "-p",
         "--pages",
-        help="",
+        help="Number of pages to scrape",
         type=int,
         default=1
     )
@@ -57,7 +68,7 @@ if __name__ == "__main__":
     pages = args.pages
     items = list(args.search)
 
+    run_time = datetime.datetime.now().strftime(STRFTIME)
     for item in items:
+        get_search_info(item=item, n_pages=pages, current_time=run_time)
         time.sleep(random.uniform(2.8423, 3.41294))
-        get_search_info(item=item, n_pages=pages)
-
